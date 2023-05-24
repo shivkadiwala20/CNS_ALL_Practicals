22d45da89a9f65aa403e4ed4b4f428166c99b6c3
Shivkadiwala_02 <110624034+shivkadiwala20@users.noreply.github.com>
2023-05-24 22:32:03 +0530

    Update 01_b_Hill.C

--- 01_b_Hill.C
+++ 01_b_Hill.C
@@ -1,120 +1,78 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include <math.h>
 
-int l, i, j;
-int temp1, temp2, temp3, n, m, count, flag, k1;
-int k[3][3];
-int p[3][1];
-int c[3][1];
-char ch;
-char pi[20];
-int choice;
 
-int check(int x, int n)
-{
-  int a, b, c;
-  if (x % n == 0)
+
+void encrypt(char *plaintext, int key[][100], int size);
+void decrypt(char *ciphertext, int key[][100], int size);
+
+int main() {
+    char input[100];
+    int size,i,j;
+int key[100][100];
+clrscr();
+    printf("Enter a string to encrypt: ");
+    fgets(input, 100, stdin);
+    printf("Enter the size of the key matrix: ");
+    scanf("%d", &size);
+
+    printf("Enter the key matrix:\n");
+    for ( i = 0; i < size; i++) {
+	for ( j = 0; j < size; j++) {
+	    scanf("%d", &key[i][j]);
+	}
+    }
+    encrypt(input, key, size);
+    printf("Encrypted string: %s\n", input);
+    decrypt(input, key, size);
+    printf("Decrypted string: %s\n", input);
+getch();
     return 0;
-
-  a = x / n;
-
-  b = n * (a + 1);
-  c = b - x;
-
-  return c;
 }
 
-int main()
-{
-  clrscr();
-  printf("\nThe Cipher has a key of length 4 and 9. Example :- A 3*3 matrix, A 2*2 matrix ");
-  printf("\n Press 3 for 3*3 matrix and Press 2 for 2*2 matrix : ");
-  scanf("%d", &n);
-  printf("\n Enter the key : ");
-
-  for (i = 0; i < n; ++i)
-  {
-    for (j = 0; j < n; ++j)
-    {
-      scanf("\n%c", &ch);
-
-      if (65 <= ch && ch <= 91)
-      {
-        k[j][i] = (int)ch % 65;
-      }
-
-      else
-        k[j][i] = (int)ch % 97;
+void encrypt(char *plaintext, int key[][100], int size) {
+    int i,j,k,len = strlen(plaintext);
+    char ciphertext[100];
+    int padding = size - (len % size);
+    for ( i = 0; i < padding; i++) {
+	plaintext[len+i] = ' ';
     }
-  }
-
-  for (i = 0; i < n; ++i)
-  {
-    for (j = 0; j < n; ++j)
-    {
-      printf("%d ", k[i][j]);
+    plaintext[len+padding] = '\0';
+    for ( i = 0; i < len+padding; i += size) {
+	for ( j = 0; j < size; j++) {
+	    int sum = 0;
+	    for ( k = 0; k < size; k++) {
+		sum += key[j][k] * (plaintext[i+k] - 'A');
+	    }
+	    ciphertext[i+j] = (sum % 26) + 'A';
+	}
     }
-    printf("\n");
-  }
+    strcpy(plaintext, ciphertext);
+}
 
-  printf("\nEnter the length of string to be encoded (without spaces): ");
-  scanf("%d", &l);
-
-  temp1 = check(l, n);
-
-  if (temp1 > 0)
-    printf("You have to enter %d bogus characters.", temp1);
-
-  printf("\nEnter the string : ");
-  for (i = -1; i < l + temp1; ++i)
-  {
-    scanf("%c", &pi[i]);
-  }
-
-  temp2 = l;
-  m = (l + temp1) / n;
-  flag = 0;
-
-  printf("\n\nThe Encoded Cipher is : ");
-
-  while (m > 0)
-  {
-    count = 0;
-
-    for (i = flag; i < flag + n; ++i)
-    {
-      if (65 <= pi[i] && pi[i] <= 91)
-        temp3 = (int)pi[i] % 65;
-
-      else
-        temp3 = (int)pi[i] % 97;
-
-      p[0][count] = temp3;
-      count = count + 1;
+void decrypt(char *ciphertext, int key[][100], int size) {
+    int len = strlen(ciphertext),i,j,k,sum=0,padding=0;
+    char plaintext[100];
+    for (i = 0; i < len; i += size) {
+	for ( j = 0; j < size; j++) {
+	     sum = 0;
+	    for ( k = 0; k < size; k++) {
+		sum += key[k][j] * (ciphertext[i+k] - 'A');
+	    }
+	    plaintext[i+j] = (sum % 26) + 'A';
+	}
     }
 
-    for (i = 0; i < n; ++i)
-      c[0][i] = 0;
-
-    for (i = 0; i < n; ++i)
-    {
-      for (j = 0; j < 1; ++j)
-      {
-        for (k1 = 0; k1 < n; ++k1)
-          c[j][i] += p[j][k1] * k[k1][i];
-      }
+    for ( i = len-1; i >= 0; i--) {
+	if (plaintext[i] == ' ') {
+	    padding++;
+	} else {
+	    break;
+	}
     }
-
-    for (i = 0; i < n; ++i)
-    {
-      c[0][i] = c[0][i] % 26;
-      printf("%c ", (char)(c[0][i] + 65));
-    }
-
-    m = m - 1;
-    flag = flag + n;
-  }
-
-  getch();
-  return 0;
+    plaintext[len-padding] = '\0';
+    strcpy(ciphertext, plaintext);
 }
